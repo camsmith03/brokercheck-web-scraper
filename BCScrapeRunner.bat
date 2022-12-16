0176b2a4dab903607ed36e9624b8860891605aae
camerons03 <camerons03@vt.edu>
2022-12-16 14:32:08 -0500

    Created main directory for program

--- BCScrapeRunner.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-python brokercheck_scraper.py
--- setup.bat
+++
@@ -1,9 +0,0 @@
-@echo on
-pip install --upgrade pip
-pip install pandas
-pip install numpy
-pip install selenium
-pip install tk
-pip install bs4
-pip install openpyxl
-pip install requests
